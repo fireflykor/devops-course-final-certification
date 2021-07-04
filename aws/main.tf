1bdcd70a1db3d902ecb94a837f014d669efe9cec
Vadim Korotaev <47751026+fireflykor@users.noreply.github.com>
2021-07-04 14:10:14 +0500

    Delete the commented code

--- aws/main.tf
+++ aws/main.tf
@@ -20,65 +20,10 @@ module "vpc" {
 }
 
 module "ec2" {
-  source    = "./modules/ec2"
+  source = "./modules/ec2"
   subnet_id = module.vpc.public_subnet_id
   sg_ids    = [module.vpc.public_sg_id]
-  name      = "web"
+  name = "web"
   env       = "devops_course"
   key_name  = "devops-cource"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//terraform {
-// required_providers {
-//    aws = {
-//      source  = "hashicorp/aws"
-//      version = "~> 3.27"
-//    }
-//  }
-//  required_version = ">= 0.12.31"
-//}
-//
-//provider "aws" {
-//  profile = "default"
-//  region  = "eu-west-2"
-//  shared_credentials_file = "/home/vadim/.aws/credentials"
-//}
-//
-//resource "aws_instance" "test-instance" {
-//  ami           = "ami-0194c3e07668a7e36"
-//  instance_type = "t2.micro"
-//  key_name = "devops-cource"
-//  security_groups = ["access-ssh-from-public"]
-//
-//  count = 2
-//
-//  tags = {
-//    Name = "InstanceFromTerraform"
-//  }
-//}
