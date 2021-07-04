0758f257ea63ffedbc14a6e5c6ae19f2d549eb26
Vadim Korotaev <47751026+fireflykor@users.noreply.github.com>
2021-07-04 17:06:56 +0500

    Add AWS and terraform validate (#3)
    * Add code for create vpc and ec2
    
    * Update terraform.yml
    
    * Add terraform init for validation
    
    * Add checkout in terraform action
    
    * Add working-directory for some steps
    
    Co-authored-by: Vadim <firefly@gmail.com>

---
+++ aws/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.12.31"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  env    = "devops_course"
+  az     = "eu-west-2a"
+}
+
+module "ec2" {
+  source    = "./modules/ec2"
+  subnet_id = module.vpc.public_subnet_id
+  sg_ids    = [module.vpc.public_sg_id]
+  name      = "web"
+  env       = "devops_course"
+  key_name  = "devops-cource"
+}
