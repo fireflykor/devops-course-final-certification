f87542ee6d6d3e252329f4594000b7fc422af82d
Vadim Korotaev <47751026+fireflykor@users.noreply.github.com>
2021-07-06 17:13:51 +0500

    Workflow and vpc improvements (#6)
    * Delete private vpc
    
    * Update ansible-lint
    
    * Update ec2.tf
    
    * Update inventory.yml
    
    Co-authored-by: Vadim <firefly@gmail.com>

--- aws/modules/vpc/variables.tf
+++ aws/modules/vpc/variables.tf
@@ -6,10 +6,6 @@ variable "public_subnet" {
   default = "10.10.11.0/24"
 }
 
-variable "private_subnet" {
-  default = "10.10.10.0/24"
-}
-
 variable "nat_gw_subnet" {
   default = "10.10.9.0/24"
 }
